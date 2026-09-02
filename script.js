4424aa334099885ab3b541374bb69412334bdba8
angelalita77 <anjeanette@gmail.com>
2026-09-02 17:59:53 -0400

    Created cached element variables

---
+++ script.js
@@ -0,0 +1,8 @@
+//  Variables to Cache Elements
+const todoForm = document.getElementById('todo-form') //☑️ select ElementByID Cache
+const todoInput = document.getElementById('todo-input');
+const todoList = document.querySelector('#todo-list') //☑️ querySelector
+const errorMessage = document.querySelector('#error-message');
+const todoCount = document.getElementById('todo-count');
+
+
