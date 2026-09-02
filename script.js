2ddc8d383758adc7e1161b875f7912bd6119ee0c
angelalita77 <anjeanette@gmail.com>
2026-09-02 19:28:15 -0400

    Completed creating the function to create the To Do listed item

--- script.js
+++ script.js
@@ -5,12 +5,32 @@ const todoList = document.querySelector('#todo-list') //☑️ querySelector
 const errorMessage = document.querySelector('#error-message');
 const todoCount = document.getElementById('todo-count');
 
-const testText = "Task"
+const testText = "Task Entered"
 
 // Helper Function - Create Todo Item
+// Output: <li> 
+//             <span>text</span> <button class="delete-btn">DELETE</button> 
+//         </li>
 function createTodoItem(text){
-    const li = document.createElement('li')
-    console.log(li)
+
+    // Create <li> element
+    const li = document.createElement('li'); //☑️ createElement
+    console.log(li);
+
+    // Create <span> element
+    const span = document.createElement("span"); // <span> is created to isolate styling only written task text
+    span.textContent = text; //☑️Use textContent
+    console.log(span);
+
+    // Creating the delete button with a class name
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "DELETE";
+    deleteBtn.className = "delete-btn";
+    console.log(deleteBtn);
+
+    
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
 }
 
-createTodoItem(testText)
+createTodoItem(testText);
